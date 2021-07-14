5540da25bc86c11431d1b433548a399d2956508b
Cong Manh <tcmanh2002@gmail.com>
2021-07-14 20:30:36 +0700

    Modified midterm exercise

--- midterm/teambuilder/src/Position.java
+++ midterm/teambuilder/src/Position.java
@@ -1,16 +1,3 @@
 public enum Position {
-    GK("Goal Keeper"),
-    DF("Defender"),
-    MF("Mid Fielder"),
-    FW("Foward");
-
-    private String position;
-
-    Position(String position) {
-        this.position = position;
-    }
-
-    String getValue() {
-        return position;
-    }
+    GK, DF, MF, FW;
 }
